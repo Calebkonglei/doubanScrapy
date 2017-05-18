9c5e7d3c66dafa04a06f3ccf5f0c3aa706236df7
孔磊 <18296155494@163.com>
2017-05-18 17:47:00 +0800

    link

--- doubanScrapy/spiders/douban_spider.py
+++ doubanScrapy/spiders/douban_spider.py
@@ -1,17 +1,22 @@
-from scrapy import Request, Spider
-from doubanScrapy.items import DoubanscrapyItem 
+from scrapy import Request
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from doubanScrapy.items import DoubanscrapyItem  
+from scrapy.loader import ItemLoader
+from scrapy.linkextractors import LinkExtractor
 
-class DoubanScrapy(Spider):
+class DoubanScrapy(CrawlSpider):
 	"""docstring for DoubanScrapy"""
 	name = 'douban'
 	start_urls = ["https://movie.douban.com/top250"]
 
-	def start_request(self):
-		yield Request(self.start_urls, callback = self.parse)
+	#定义抓取规则
+	rules = [
+		Rule(LinkExtractor(allow = (r"https://movie.douban.com/top250\?start=\d+\&filter=")), callback = 'parse'),
+		Rule(LinkExtractor(allow = (r"https://movie.douban.com/top250")), follow = True)	
+	]
 
 	def parse(self, response):
 		for msg in response.xpath('//div[@class="item"]'):
-
 			item = DoubanscrapyItem()
 			item['url'] = msg.xpath('div[@class="pic"]/a/@href').extract()[0]
 			item['rank'] = msg.xpath('div[@class="pic"]/em/text()').extract()[0]
@@ -20,6 +25,7 @@ class DoubanScrapy(Spider):
 			comment = msg.xpath('div[@class="info"]/div[@class="bd"]/p[@class="quote"]/span/text()').extract()
 			if comment:
 				item['comment'] = comment[0]
+
 			yield item
 
 		next_page = response.xpath('//span[@class="next"]/a/@href').extract_first()
